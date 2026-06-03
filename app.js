b866dc6e171241d4fc03ad45b93ca4c6f4ac8639
Asmaa <asmaamounsif.dev@gmail.com>
2026-06-03 14:28:53 +0100

    verification

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 var heading = document.createElement("h1");
 heading.textContent = "Hello my name is asmaa";
 document.body.appendChild(heading);
+
+var paragraph = document.createElement("p");
+paragraph.textContent = "This is a paragraph.";
+document.body.appendChild(paragraph);
